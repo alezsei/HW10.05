b8b4733d809b59ed02c00d0242e1f23a96d3139b
alezsei <alezsei@yandex.ru>
2023-06-10 20:50:11 +0400

    Task25

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int InText(string Text)
+{
+    Console.WriteLine(Text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Task25()
+{
+    int A = InText("Введите основание степени");
+    int B = InText("Введите степень числа");
+    Console.WriteLine($"число {A} в степени {B} = {Math.Pow(A,B)}");
+
+}
+
+
+Task25();
